013b4dab3ad6f2159b6ca4c15e35c8ec3effa28d
Charles Mueller <crmueller100@gmail.com>
2024-07-07 23:42:24 -0500

    created bar chart for white vs. black performance

--- app/queries.py
+++ app/queries.py
@@ -5,52 +5,103 @@ from datetime import datetime
 # TODO: delete this 
 client, db, collection  = connect_to_mongo()
 
-def get_all_games(collection, player):
-    return collection.count_documents({"player": player})
+def get_all_games(collection, player, time_class=None, color=None):
+    filter_query = { "player": player }
+    if time_class:
+        filter_query["time_class"] = time_class
+    if color:
+        filter_query[f"{color}.username"] = {"$regex": f"^{player}$", "$options": "i"}
 
-def get_all_games_as_white(collection, player):
+    return collection.count_documents(filter_query)
+
+def get_all_games_as_white(collection, player, time_class=None, color=None):
     filter_query = {
         # makes it a case-insensitive search
         "white.username": {"$regex": f"^{player}$", "$options": "i"},
         "player": player
     }
+    if time_class:
+        filter_query["time_class"] = time_class
+    if color:
+        filter_query[f"{color}.username"] = {"$regex": f"^{player}$", "$options": "i"}
+
     return collection.count_documents(filter_query)
 
-def get_all_games_as_black(collection, player):
+def get_all_games_as_black(collection, player, time_class=None, color=None):
     filter_query = {
         # makes it a case-insensitive search
         "black.username": {"$regex": f"^{player}$", "$options": "i"},
         "player": player
     }
+    if time_class:
+        filter_query["time_class"] = time_class
+    if color:
+        filter_query[f"{color}.username"] = {"$regex": f"^{player}$", "$options": "i"}
+
     return collection.count_documents(filter_query)
 
-def get_all_games_played_in_a_month(collection, player, month):
+def get_all_games_played_in_a_month(collection, player, month, time_class=None, color=None):
     filter_query = {
         "player": player,
         "month": month
     }
+    if time_class:
+        filter_query["time_class"] = time_class
+    if color:
+        filter_query[f"{color}.username"] = {"$regex": f"^{player}$", "$options": "i"}
 
     return collection.count_documents(filter_query)
 
 
-# def get_half_games():
-#     """Fetches all games from the collection."""
-#     return 5
+def get_win_loss_counts(collection, player, color, time_class=None, color_filter=None):
 
+    pipeline = [
+        {
+            "$match": {
+                f"{color}.username": {"$regex": player, "$options": "i"},
+                **({"time_class": time_class} if time_class else {}),
+                **({f"{color_filter}.username": {"$regex": f"^{player}$", "$options": "i"}} if color_filter else {})
+                
+            }
+        },
+        {
+            # This creates a new field called "result" that contains the value of "{color}.result"
+            "$project": { 
+                "result": f"${color}.result"
+            }
+        },
+        {
+            "$group": {
+                "_id": "$result",
+                "count": {"$sum": 1}
+            }
+        }
+    ]
 
+    result = list(collection.aggregate(pipeline))
+    return result
 
-# from datetime import datetime
+def count_time_controls(collection, player, time_class=None, color=None):
+    pipeline = [
+        {
+            "$match": {
+                f"player": {"$regex": player, "$options": "i"},
+                **({"time_class": time_class} if time_class else {}),
+                **({f"{color}.username": {"$regex": f"^{player}$", "$options": "i"}} if color else {})
+            }
+        },
+        {
+            "$project": { 
+                "result": "$time_class"
+            }
+        },
+        {
+            "$group": {
+                "_id": "$result",
+                "count": {"$sum": 1}
+            }
+        }
+    ]
 
-# def get_games_this_month(collection):
-#     """Fetches games played in the current month."""
-
-#     now = datetime.now()
-#     start_of_month = datetime(now.year, now.month, 1)  
-#     return 'helo'
-#     # games = []
-#     # for doc in collection.find():
-#     #     for game in doc["games"]:
-#     #         game_end_time = datetime.utcfromtimestamp(game["end_time"])  # Convert to datetime
-#     #         if game_end_time >= start_of_month:
-#     #             games.append(game)
-#     # return games
+    result = list(collection.aggregate(pipeline))
+    return result
--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -4,32 +4,41 @@ import datetime
 from dateutil.relativedelta import relativedelta
 
 import plotly.express as px
+import plotly.graph_objects as go
+
 
 # Connect to MongoDB
 client, db, collection = connect_to_mongo()
 
 with st.sidebar:
-    # TODO: Eventually make it a multiselect (different data structure than single select): https://docs.streamlit.io/develop/api-reference/widgets/st.multiselect
     player = st.text_input("Enter player username:")
-    time_class = st.selectbox("Time Control", ["All","bullet", "blitz", "daily"])
+    time_class = st.selectbox("Time Control", ["All","bullet", "blitz", "daily"]) # TODO: Eventually make it a multiselect (different data structure than single select): https://docs.streamlit.io/develop/api-reference/widgets/st.multiselect
+    color = st.radio("Color", ["All","White", "Black"]).lower()
+
+player = 'hikaru' # TODO: remove. This is just for testing
+print('\n\n\n\n')
 
 if not player:
     st.error("Please enter a player username")
     st.stop()
+
 if time_class == "All":
     time_class = None
 
+if color == "all":
+    color = None
+
 st.title(f"Chess Stats for {player}")
 
-all_games = get_all_games(collection, player, time_class)
-all_games_as_white = get_all_games_as_white(collection, player, time_class)
-all_games_as_black = get_all_games_as_black(collection, player, time_class)
+all_games = get_all_games(collection, player, time_class, color)
+all_games_as_white = get_all_games_as_white(collection, player, time_class, color)
+all_games_as_black = get_all_games_as_black(collection, player, time_class, color)
 
 current_month = datetime.datetime.now().strftime('%Y_%m')
 last_month = (datetime.datetime.now() - relativedelta(months=1)).strftime('%Y_%m')
 
-all_games_played_this_month = get_all_games_played_in_a_month(collection, player, current_month, time_class)
-all_games_played_last_month = get_all_games_played_in_a_month(collection, player, last_month, time_class)
+all_games_played_this_month = get_all_games_played_in_a_month(collection, player, current_month, time_class, color)
+all_games_played_last_month = get_all_games_played_in_a_month(collection, player, last_month, time_class, color)
 
 col1, col2, col3, col4 = st.columns(4)
 col1.metric(label="Total Games Played", value=all_games)
@@ -39,18 +48,19 @@ col4.metric(label="Games Played This Month", value=all_games_played_this_month, 
 
 col1, col2, col3 = st.columns(3)
 
-results_as_white = get_win_loss_counts(collection, player, 'white', time_class)
-labels = ['lose' if doc['_id'] in('resigned','checkmated','timeout','abandoned') 
+results_as_white = get_win_loss_counts(collection, player, 'white', time_class, color)
+labels_white = ['lose' if doc['_id'] in('resigned','checkmated','timeout','abandoned') 
           else "draw" if doc["_id"] in ('agreed','repetition','insufficient','stalemate') 
           else doc["_id"] for doc in results_as_white]
-values = [doc['count'] for doc in results_as_white]
+
+values_white = [doc['count'] for doc in results_as_white]
 
 fig = px.pie(
-    names=labels,
-    values=values,
+    names=labels_white,
+    values=values_white,
     title=f"Game Results as White",
     labels={'names': 'Result', 'values': 'Count'},
-    color=labels,
+    color=labels_white,
     color_discrete_map={
         "win": "#90EE90",
         "lose": "#DD5C5C",
@@ -60,18 +70,20 @@ fig = px.pie(
 
 col1.plotly_chart(fig)
 
-results_as_white = get_win_loss_counts(collection, player, 'black', time_class)
-labels = ['lose' if doc['_id'] in('resigned','checkmated','timeout','abandoned') 
+results_as_white = get_win_loss_counts(collection, player, 'black', time_class, color)
+labels_black = ['lose' if doc['_id'] in('resigned','checkmated','timeout','abandoned') 
           else "draw" if doc["_id"] in ('agreed','repetition','insufficient','stalemate') 
           else doc["_id"] for doc in results_as_white]
-values = [doc['count'] for doc in results_as_white]
+
+values_black = [doc['count'] for doc in results_as_white]
+
 
 fig = px.pie(
-    names=labels,
-    values=values,
+    names=labels_black,
+    values=values_black,
     title=f"Game Results as Black",
     labels={'names': 'Result', 'values': 'Count'},
-    color=labels,
+    color=labels_black,
     color_discrete_map={
         "win": "#90EE90",
         "lose": "#DD5C5C",
@@ -81,11 +93,11 @@ fig = px.pie(
 
 col2.plotly_chart(fig)
 
-count_time_controls = count_time_controls(collection, player, time_class)
+count_time_controls = count_time_controls(collection, player, time_class, color)
 
 labels = [doc["_id"] for doc in count_time_controls]
 values = [doc["count"] for doc in count_time_controls]
-print(labels)
+
 fig = px.pie(
     names=labels,
     values=values,
@@ -95,4 +107,17 @@ fig = px.pie(
 
 col3.plotly_chart(fig)
 
-# st.metric(label="Total Games Played", value=50)
+
+fig = px.bar(
+    x=labels_white + labels_black, 
+    y=values_white + values_black,
+    color=["White"] * len(labels_white) + ["Black"] * len(labels_black),  # Color list
+    barmode="group",
+    title="Game Results by Color",
+    labels={"x": "Result", "y": "Number of Games", "color": "Player Color"}
+)
+
+# Display chart
+st.plotly_chart(fig)
+
+st.metric(label="Total Games Played", value=50)
