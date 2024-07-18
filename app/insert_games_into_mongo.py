037ee260f3ebff38cca9600633cea2e86c27a9a9
Charles Mueller <crmueller100@gmail.com>
2024-07-17 20:25:16 -0500

    reduced duplicate attempts on uploading games to mongo

--- app/insert_games_into_mongo.py
+++ app/insert_games_into_mongo.py
@@ -2,32 +2,47 @@ import os
 import json
 import pymongo
 from pymongo.errors import DuplicateKeyError
+from pprint import pprint
 
+from datetime import datetime
 
+def get_latest_game_in_database(client, db, collection, player):
+    latest_month = collection.find_one({'player': player.lower()}, sort=[('month', pymongo.DESCENDING)], projection={"month": 1})
+    return latest_month['month']
 
 def insert_games_into_mongo(client, db, collection, player):
-    # TODO: Don't need to try to insert the entire history every time. Maybe use latest month as a starting point and only insert new data.
-    # Probably could add another value to the config.yaml file to determine if we want to insert the whole history
     rootdir = f"../data/game_archives/{player}"
 
+    latest_month = get_latest_game_in_database(client, db, collection, player) # formated as YYYY_MM
+    if not latest_month:
+        latest_month = '2000_01' # If no games are found, start earlier than chess.com was founded
+
     for monthly_games in os.listdir(rootdir):
-        try:
-            with open(f"{rootdir}/{monthly_games}", "r") as f:
-                data = json.load(f)
-                games = data.get("games", [])
-                yyyy_mm = monthly_games[:-5]
-                
-                insert_count = 0
-                duplicate_count = 0
-                for game in games:
-                    try:
-                        # The URL will be the default unique identifier (_id). Could use the UUID with collection.insert_one({"_id": game["uuid"], **game}) 
-                        collection.insert_one({"_id": game["uuid"], "player": player.lower(), "month": yyyy_mm, **game}) 
-                        # print(f"Inserted yyyy_mm {yyyy_mm} for player {player}")
-                        insert_count += 1
-                    except DuplicateKeyError:
-                        # print(f"Duplicate game found (skipping): {game.get('url')}")  
-                        duplicate_count += 1
-                print(f"Inserted {insert_count} games and skipped {duplicate_count} duplicates for {player} in {yyyy_mm}")
-        except FileNotFoundError:
-            print(f"File not found: {monthly_games}")
+        yyyy_mm = monthly_games[:-5]
+        
+        yyyy_mm_datetime = datetime.strptime(yyyy_mm, "%Y_%m")
+        latest_month_datetime = datetime.strptime(latest_month, "%Y_%m")
+        
+        if yyyy_mm_datetime >= latest_month_datetime: # If the directory is the same or newer than the latest month in the database, insert the games
+            try:
+                with open(f"{rootdir}/{monthly_games}", "r") as f:
+                    file_content = f.read().strip()
+                    if file_content: # Check if the file is empty
+                        f.seek(0)
+                        data = json.load(f)
+                        games = data.get("games", [])
+                        
+                        insert_count = 0
+                        duplicate_count = 0
+                        for game in games:
+                            try:
+                                # The URL will be the default unique identifier (_id). Could use the UUID with collection.insert_one({"_id": game["uuid"], **game}) 
+                                collection.insert_one({"_id": game["uuid"], "player": player.lower(), "month": yyyy_mm, **game}) 
+                                # print(f"Inserted yyyy_mm {yyyy_mm} for player {player}")
+                                insert_count += 1
+                            except DuplicateKeyError:
+                                # print(f"Duplicate game found (skipping): {game.get('url')}")  
+                                duplicate_count += 1
+                        print(f"Inserted {insert_count} games and skipped {duplicate_count} duplicates for {player} in {yyyy_mm}")
+            except FileNotFoundError:
+                print(f"File not found: {monthly_games}")
