85f83eb0888692212de5163785a992bac499ea7e
Charles Mueller <crmueller100@gmail.com>
2024-08-07 22:35:03 -0500

    insert player_expectation into document

--- app/stockfish_analysis.py
+++ app/stockfish_analysis.py
@@ -13,7 +13,13 @@ async def analyze_wdl_with_stockfish() -> None:
     x = collection.find_one()
     pgn = x['pgn']
     player_color = 'white' if x['player'].lower() == x['white']['username'].lower() else 'black'
-    # pprint(x)
+
+    existing_data = x['player_expectation'] if 'player_expectation' in x else None
+
+    # If the analysis has already been done, don't do it again. 
+    if existing_data:
+        return
+
     game = chess.pgn.read_game(io.StringIO(pgn))  
 
     STOCKFISH_PATH = os.getenv("STOCKFISH_PATH")
@@ -24,9 +30,10 @@ async def analyze_wdl_with_stockfish() -> None:
     white_expectation = []
     black_expectation = []
 
+    print("Analyzing game...")
     for move in game.mainline_moves():
         # Don't set the time parameter smaller than 0.1. It will cause the engine to think one side is winning when it isn't because it can't calculate far enough ahead
-        info = await engine.analyse(board, chess.engine.Limit(time=0.01)) # TODO: Change this back to 0.1
+        info = await engine.analyse(board, chess.engine.Limit(time=0.1))
 
         white_expectation.append(info["score"].white().wdl().expectation())
         black_expectation.append(info["score"].black().wdl().expectation())
@@ -34,14 +41,11 @@ async def analyze_wdl_with_stockfish() -> None:
         board.push(move)
     await engine.quit()
 
-    # print("\n\nWhite's expectation of winning:", white_expectation)
-    # print("Black's expectation of winning:", black_expectation)
-
-
     player_expectation = white_expectation if player_color == 'white' else black_expectation
+    # print("Players's expectation of winning:", player_expectation)
     
     filter_query = {'_id': x['_id']}
-    update_operation = {'$set': {f"player_expectation": player_expectation}}
+    update_operation = {'$setOnInsert': {f"player_expectation": player_expectation}}
 
     result = collection.update_one(filter_query, update_operation, upsert=False)
 
@@ -49,7 +53,7 @@ async def analyze_wdl_with_stockfish() -> None:
     if result.modified_count > 0:
         print(f"Updated document with _id: {x['_id']} for {player_color}")
     else:
-        print(f"Failed to update document with _id: {x['_id']} for {player_color}")
+        print(f"No changes made to document with _id: {x['_id']} for {player_color} (player_expectation might already exist)")
 
     client.close()
 
