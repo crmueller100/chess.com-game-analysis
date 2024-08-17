80d39835f1baf6a4102ce10c94a80a76929f8934
Charles Mueller <crmueller100@gmail.com>
2024-08-16 22:51:20 -0500

    stockfish analysis uses config.yaml

--- app/stockfish_analysis.py
+++ app/stockfish_analysis.py
@@ -4,20 +4,36 @@ import chess.engine
 import os 
 import io
 from pprint import pprint
+import yaml
 
 import chess.pgn 
 from connect_to_mongo import connect_to_mongo
 
 async def analyze_wdl_with_stockfish() -> None:
-    client, db, collection  = connect_to_mongo()
-    x = collection.find_one()
-    pgn = x['pgn']
-    player_color = 'white' if x['player'].lower() == x['white']['username'].lower() else 'black'
+    with open(config_path, 'r') as file:
+        config = yaml.safe_load(file)
+    
+    player = config.get('username')
+    stockfish_time_limit = config.get('stockfish_time_limit', 0.1)
+    overwrite_stockfish_analysis = config.get('overwrite_stockfish_analysis', False)
 
-    existing_data = x['player_expectation'] if 'player_expectation' in x else None
+    client, db, collection  = connect_to_mongo()
+
+    # TODO: Make this a configurable value
+    g = collection.find_one({'player': player})
+    
+    if g is None:
+        print(f"No game data found for player {player}")
+        return
+
+    pgn = g['pgn']
+    player_color = 'white' if g['player'].lower() == g['white']['username'].lower() else 'black'
+
+    existing_data = g['player_expectation'] if 'player_expectation' in g else None
 
     # If the analysis has already been done, don't do it again. 
     if existing_data:
+        print(f"Stockfish analysis already done for the selected game with {player} as {player_color}")
         return
 
     game = chess.pgn.read_game(io.StringIO(pgn))  
@@ -32,8 +48,7 @@ async def analyze_wdl_with_stockfish() -> None:
 
     print("Analyzing game...")
     for move in game.mainline_moves():
-        # Don't set the time parameter smaller than 0.1. It will cause the engine to think one side is winning when it isn't because it can't calculate far enough ahead
-        info = await engine.analyse(board, chess.engine.Limit(time=0.1))
+        info = await engine.analyse(board, chess.engine.Limit(time=stockfish_time_limit))
 
         white_expectation.append(info["score"].white().wdl().expectation())
         black_expectation.append(info["score"].black().wdl().expectation())
@@ -44,19 +59,26 @@ async def analyze_wdl_with_stockfish() -> None:
     player_expectation = white_expectation if player_color == 'white' else black_expectation
     # print("Players's expectation of winning:", player_expectation)
     
-    filter_query = {'_id': x['_id']}
-    update_operation = {'$setOnInsert': {f"player_expectation": player_expectation}}
+    filter_query = {'_id': g['_id']}
+
+    if overwrite_stockfish_analysis: 
+        # Use $set to unconditionally update (or create) the field
+        update_operation = {'$set': {f"player_expectation": player_expectation}}
+    else:
+        update_operation = {'$setOnInsert': {f"player_expectation": player_expectation}}
 
     result = collection.update_one(filter_query, update_operation, upsert=False)
 
     print(f"modified_count: {result.modified_count}")
     if result.modified_count > 0:
-        print(f"Updated document with _id: {x['_id']} for {player_color}")
+        print(f"Updated document with _id: {g['_id']} for {player_color}")
     else:
-        print(f"No changes made to document with _id: {x['_id']} for {player_color} (player_expectation might already exist)")
+        print(f"No changes made to document with _id: {g['_id']} for {player_color} (player_expectation might already exist)")
 
+    
     client.close()
 
 if __name__ == "__main__":
+    config_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),'config.yaml')
     asyncio.run(analyze_wdl_with_stockfish())
 
