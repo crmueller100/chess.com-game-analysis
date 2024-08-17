6c776b5745abca2aab235ce15a84ef77ed4dd31e
Charles Mueller <crmueller100@gmail.com>
2024-08-17 13:15:45 -0500

    working, scrollable table on second dashboard

--- dashboard/pages/2_Stockfish_Analysis.py
+++ dashboard/pages/2_Stockfish_Analysis.py
@@ -1,15 +1,19 @@
 import streamlit as st
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
+from time import strftime, localtime
 
 import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
 from collections import defaultdict
+from pprint import pprint
+
+import pandas as pd
+import numpy as np
 
 from queries import *
-
 
 # Connect to MongoDB
 client, db, collection = connect_to_mongo()
@@ -20,8 +24,8 @@ with st.sidebar:
     color = st.radio("Color", ["All","White", "Black"]).lower()
     date = st.date_input('Start Date', value=None, min_value=datetime(2005,1,1))
 
-# player = 'hikaru'
-# print('\n\n\n\n')
+player = 'hikaru'
+print('\n\n\n\n')
 
 if not player:
     st.error("Please enter a player username")
@@ -40,11 +44,41 @@ if not collection.find_one({"player": player}):
     st.stop()
 
 
-
 #########################################################
 # Query Mongo and collect all the data
 #########################################################
 
+games = collection.find({'player': player}).limit(100)
+pprint(collection.find_one({'player': player}))
+
+keys_to_display = ["_id", "url", "time_control"]
+
+table_data = []
+
+# Extract values for the specified keys from each game document
+for game in games:
+    row_data = {key: game.get(key) for key in keys_to_display}
+    row_data['Winner'] = game.get('white', {}).get('username') if game.get('white', {}).get('result') == 'win' else game.get('black', {}).get('username')
+    row_data['White'] = game.get('white', {}).get('username')
+    row_data['White Rating'] = game.get('white', {}).get('rating')
+    row_data['Black'] = game.get('black', {}).get('username')
+    row_data['Black Rating'] = game.get('black', {}).get('rating')
+
+    start_time = game.get('start_time')
+    if start_time:
+        row_data['Start Date'] = datetime.fromtimestamp(start_time).strftime('%Y-%m-%d')
+    else:
+        row_data['Start Date'] = 'N/A'
+
+    table_data.append(row_data)
+
+df = pd.DataFrame(table_data)
+
+# Convert the rating columns to strings to remove commas
+df['White Rating'] = df['White Rating'].astype(str)
+df['Black Rating'] = df['Black Rating'].astype(str)
+
+st.dataframe(df)
 
 
 #########################################################
@@ -52,9 +86,9 @@ if not collection.find_one({"player": player}):
 #########################################################
 
 
-
 #########################################################
 # Assemble Dashboard
 #########################################################
+
 st.title(f"Stockfish Analysis for {player}")
 
