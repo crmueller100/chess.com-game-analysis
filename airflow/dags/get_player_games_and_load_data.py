6d742d8c0a988bef53ca71cea2313f16f22ac838
Charles Mueller <crmueller100@gmail.com>
2024-12-28 00:03:44 -0600

    not working; can't refute default values in get_player_games_and_load_data

--- airflow/dags/get_player_games_and_load_data.py
+++ airflow/dags/get_player_games_and_load_data.py
@@ -1,7 +1,6 @@
 from airflow.models.dag import DAG
 from airflow.operators.python import PythonOperator
 from airflow.models.param import Param
-
 from datetime import datetime
 
 from get_player_data import get_player_data, get_player_game_archives, save_player_game_archives
@@ -9,48 +8,48 @@ from connect_to_mongo import connect_to_mongo
 from insert_games_into_mongo import insert_games_into_mongo
 
 def save_game_archives_wrapper(player, **kwargs):
-    # Retrieve the game archives from XCom
     ti = kwargs['ti']
-    game_archives = ti.xcom_pull(task_ids='check_game_archives')
+    game_archives = ti.xcom_pull(task_ids=f'check_game_archives_{player}')
     save_player_game_archives(player, game_archives)
 
 
-def load_data_to_mongo_wrapper(**kwargs):
-    player = kwargs['params']['player_username']
+def load_data_to_mongo_wrapper(player, **kwargs):
     client, db, collection = connect_to_mongo()
     insert_games_into_mongo(client, db, collection, player)
 
 
 with DAG(
     dag_id="get_player_games_and_load_data",
-    start_date=datetime.now(),
+    start_date=datetime(2024, 1, 1),  # Use a fixed start date
     schedule_interval="@monthly",
     params={
-        "player_username": Param(default="hikaru", type="string", title="Enter in a player's username")
+        "player_usernames": Param(default=['hikaru', 'cmuell'], type="array", title="Enter player usernames. Separate each name with a newline."),
     },
 ) as dag:
-    t1 = PythonOperator(
-        task_id="check_player_exists",
-        python_callable=get_player_data,
-        op_kwargs={"player": "{{ params.player_username }}"},
-    )
-    t2 = PythonOperator(
-        task_id="check_game_archives",
-        python_callable=get_player_game_archives,
-        op_kwargs={"player": "{{ params.player_username }}"},
-        do_xcom_push=True,  # Need to pass the game archives to the next task
-    )
-    t3 = PythonOperator(
-        task_id="save_game_archives",
-        python_callable=save_game_archives_wrapper,
-        op_kwargs={"player": "{{ params.player_username }}"},
-        provide_context=True,  # Allows passing the task instance (ti) to the callable
-    )
-    t4 = PythonOperator(
-        task_id="load_data_to_mongo",
-        python_callable=load_data_to_mongo_wrapper,
-        op_kwargs={"player": "{{ params.player_username }}"},
-        provide_context=True,
-    )
+    players = dag.params["player_usernames"]  # Resolve params to a Python list
+    for player in players:
+        sanitized_player = player.strip()  # Sanitize player names for valid task IDs
+        t1 = PythonOperator(
+            task_id=f"check_player_exists_{sanitized_player}",
+            python_callable=get_player_data,
+            op_kwargs={"player": sanitized_player},  # Pass the individual player
+        )
+        t2 = PythonOperator(
+            task_id=f"check_game_archives_{sanitized_player}",
+            python_callable=get_player_game_archives,
+            op_kwargs={"player": sanitized_player},  # Pass the individual player
+            do_xcom_push=True,
+        )
+        t3 = PythonOperator(
+            task_id=f"save_game_archives_{sanitized_player}",
+            python_callable=save_game_archives_wrapper,
+            op_kwargs={"player": sanitized_player},  # Pass the individual player
+            provide_context=True,
+        )
+        t4 = PythonOperator(
+            task_id=f"load_data_to_mongo_{sanitized_player}",
+            python_callable=load_data_to_mongo_wrapper,
+            op_kwargs={"player": sanitized_player},  # Pass the individual player
+        )
 
-    t1 >> t2 >> t3 >> t4
+        t1 >> t2 >> t3 >> t4
